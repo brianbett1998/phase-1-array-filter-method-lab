92d719e593c1c57e472f1ec46e1d805235a7e6c4
BRIAN BETT <+>
2023-03-23 12:16:38 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']("")
+function findMatching(drivers, query) {
+    return drivers.filter(driver=>driver.toLowercase()===query.toLowercase()
+    );
+    describe('findMatching()', function () {
+      it('returns all drivers that match the passed in name', function () {
+        const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+  
+        expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
+        expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
+      });
+  
+      it('returns matching drivers if case does not match but letters do', function () {
+        const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+  
+        expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'bobby']);
+      });
+  
+      it('returns an empty array if there is no match', function () {
+        const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+  
+        expect(findMatching(drivers, 'Susan')).to.eql([]);
+      });
+    });
+  
+
+}
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
